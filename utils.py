7f06bbf5fec7abc1fb91aa9ac3321fed37353711
1sgp <138119976+1sgp@users.noreply.github.com>
2023-09-28 10:26:34 +0200

    0.4

--- utils.py
+++ utils.py
@@ -4,30 +4,32 @@ from datetime import datetime as dt
 from datetime import timedelta as td
 from getpass import getpass as gp
 from os import name, system
-from time import sleep, time
+from time import sleep
+from random import randint
+
+class Times:
+    start_minmin = 10        #Minimum Minute for randominzing Start (8:<start_minmin> = 8:00) - Change here if needed - ex.: set to 15 for a random login time between 8:15 and 8:30. (default 0)
+    start_maxmin = 26       #Minimum Minute for randominzing Start (8:<start_maxmin> = 8:30) - Change here if needed - ex.: set to 15 for a random login time between 8:00 and 8:15. (default 29)
+    end_minmin = 31         #Minimum Minute for randominzing Feierabend (16:<end_minmin> = 16:31) - Change here if needed
+    end_maxmin = 35         #Maximum Minute for randominzing Feierabend (16:<end_maxmin> = 16:35) - Change here if needed
+    start_time = 0           # 08:30 gets updated within daily while loop
+    end_time = 0             # 16:15 gets updated within daily while loop
+    start_mintime = 0        # dt(start_time.year, start_time.month, start_time.day, start_time.hour, start_minmin)
+    start_maxtime = 0        # dt(start_time.year, start_time.month, start_time.day, start_time.hour, start_maxmin)
+    end_mintime = 0         
+    end_maxtime = 0
+    @classmethod
+    def update(self):
+        x = dt.now()
+        self.start_time = dt(x.year, x.month, x.day, 8, 30)
+        self.end_time = dt(x.year, x.month, x.day, 16, 15)
+        self.start_mintime = dt(self.start_time.year, self.start_time.month, self.start_time.day, self.start_time.hour, self.start_minmin)
+        self.start_maxtime = dt(self.start_time.year, self.start_time.month, self.start_time.day, self.start_time.hour, self.start_maxmin)
+        self.end_mintime = dt(self.end_time.year, self.end_time.month, self.end_time.day, self.end_time.hour, self.end_minmin)
+        self.end_maxtime = dt(self.end_time.year, self.end_time.month, self.end_time.day, self.end_time.hour, self.end_maxmin)
 
 def clear():
     system('cls' if name == 'nt' else 'clear')
-
-def countdown(x):
-    # A countdown that accepts seconds or a time in "HH:MM"-format as input.
-    s = time()                  # Test
-    now = dt.now()
-    if type(x) == str:
-        untiltime = dt.strptime(x, '%H:%M')
-        until = dt(now.year, now.month, now.day, untiltime.hour, untiltime.minute)
-        delta = (until - now).total_seconds()
-        x = round(delta)
-    else:
-        delta = td(seconds=x)
-        until = now + delta
-    for i in range(x,1,-1):
-        print(f"Sleeping until {until.isoformat()} or {i} seconds.         ", end="\r")
-        sleep(0.995)
-    print(f"Sleeping until {until.isoformat()} or 1 second.                   ", end="\r")
-    sleep(0.995)
-    print(f"                                                                      ", end="\r")
-    print(f"Done after {time()-s:.2f} seconds")         # Test
 
 def enter_creds():
     # Prompts for Credentials. name@domain.tld and a password with a minimum length of four (non-blank) characters
@@ -59,3 +61,21 @@ def enter_creds():
         return User, Pass
     else:
         return f"Exceeded maximum tries. Please do better next time!"
+    
+def RandStartZeit():
+    now = dt.now()
+    if now < Times.start_mintime:
+        randi = dt(now.year,now.month,now.day,8,randint(Times.start_minmin,Times.start_maxmin))
+    elif now > Times.start_mintime and now < Times.start_maxtime:
+        randi = dt(now.year,now.month,now.day,8,randint(now.minute,Times.start_maxmin))
+    else:
+        randi = now + td(seconds=3)
+    return randi
+
+def RandEndZeit():
+    now = dt.now()
+    if now >= Times.end_mintime:
+        randi = dt(now.year,now.month,now.day,16,randint((now.minute),Times.end_maxmin))
+    else:
+        randi = dt(now.year,now.month,now.day,16,randint(Times.end_minmin,Times.end_maxmin))
+    return randi
